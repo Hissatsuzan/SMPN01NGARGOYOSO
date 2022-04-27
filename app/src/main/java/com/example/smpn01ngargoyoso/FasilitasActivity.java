8c7e4add46d0dc9a2edb2dd09555d323bf9cc184
Hissatsuzan <xrayirf@gmail.com>
2022-04-27 12:27:14 +0700

    project smp 1

--- app/src/main/java/com/example/smpn01ngargoyoso/FasilitasActivity.java
+++ app/src/main/java/com/example/smpn01ngargoyoso/FasilitasActivity.java
@@ -2,7 +2,9 @@ package com.example.smpn01ngargoyoso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FasilitasActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class FasilitasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fasilitas);
+    }public void masjidclick(View view) {
+        Intent intent = new Intent(FasilitasActivity.this, MasjidActivity.class);
+        startActivity(intent);
+    }public void lapclick(View view) {
+        Intent intent = new Intent(FasilitasActivity.this, LapanganActivity.class);
+        startActivity(intent);
+    }public void kanclick(View view) {
+        Intent intent = new Intent(FasilitasActivity.this, KantinActivity.class);
+        startActivity(intent);
+    }public void labclick(View view) {
+        Intent intent = new Intent(FasilitasActivity.this, LaboratoriumActivity.class);
+        startActivity(intent);
+    }public void perclick(View view) {
+        Intent intent = new Intent(FasilitasActivity.this, PerpusActivity.class);
+        startActivity(intent);
     }
 }
